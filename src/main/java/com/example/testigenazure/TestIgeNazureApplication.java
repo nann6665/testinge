1867bf59ff2f31e82660166e3e984eee2adc7aed
nann6665 <nann6665@kea.stud.dk>
2022-08-26 10:19:25 +0200

    dumme deployment

---
+++ src/main/java/com/example/testigenazure/TestIgeNazureApplication.java
@@ -0,0 +1,13 @@
+package com.example.testigenazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestIgeNazureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestIgeNazureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testigenazure/TestIgeNazureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testigenazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestIgeNazureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
